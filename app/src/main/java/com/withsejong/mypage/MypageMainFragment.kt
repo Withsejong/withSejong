f18e2b5597ad7d38ba3e9cc1c42139b56c1f20af
MisterJerry <misterjerry@sju.ac.kr>
2024-04-18 18:53:28 +0900

    뷰바인딩 설정

--- app/src/main/java/com/withsejong/mypage/MypageMainFragment.kt
+++ app/src/main/java/com/withsejong/mypage/MypageMainFragment.kt
@@ -1,16 +1,20 @@
 package com.withsejong.mypage
 
-import android.content.Intent
+import android.content.Context.MODE_PRIVATE
 import android.graphics.Paint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.FragmentTransaction
 import com.withsejong.R
 import com.withsejong.databinding.FragmentMypageMainBinding
+import com.withsejong.mypage.buyList.BuyListFragment
+import com.withsejong.mypage.faq.FaqFragment
 import com.withsejong.mypage.interestingList.InterestingListFragment
+import com.withsejong.mypage.myInformation.MyInformationFragment
+import com.withsejong.mypage.sellList.SellListFragment
+import com.withsejong.mypage.setting.SettingFragment
 
 class MypageMainFragment : Fragment() {
     private lateinit var binding:FragmentMypageMainBinding
@@ -23,10 +27,28 @@ class MypageMainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val userInfoSharedPreferences = requireActivity().getSharedPreferences("userInfo",
+            MODE_PRIVATE
+        )
+        val loadedName = userInfoSharedPreferences.getString("nickname", "Error")
+
+        binding.tvName.text = "${loadedName}님"
+
         //밑줄을 위한코드
         binding.tvName.paintFlags = Paint.UNDERLINE_TEXT_FLAG
 
         val interestingListFragment = InterestingListFragment()
+        val sellListFragment = SellListFragment()
+        val buyListFragment = BuyListFragment()
+        val myInformationFragment = MyInformationFragment()
+        val settingFragment = SettingFragment()
+        val faqFragment = FaqFragment()
+
+
+
+
+
         //val intentInterstingList = Intent(requireContext(),interestingListFragment::class.java)
 
 
@@ -37,6 +59,39 @@ class MypageMainFragment : Fragment() {
                 .commit()
 
         }
+
+        binding.cloSellList.setOnClickListener{
+            val fragmentManager = parentFragmentManager
+            fragmentManager.beginTransaction()
+                .replace(R.id.fcv_all_fragment,sellListFragment)
+                .commit()
+        }
+        binding.cloBuyList.setOnClickListener {
+            val fragmentManager = parentFragmentManager
+            fragmentManager.beginTransaction()
+                .replace(R.id.fcv_all_fragment,buyListFragment)
+                .commit()
+        }
+        binding.cloMyInformation.setOnClickListener {
+            val fragmentManager = parentFragmentManager
+            fragmentManager.beginTransaction()
+                .replace(R.id.fcv_all_fragment,myInformationFragment)
+                .commit()
+        }
+        binding.cloSetting.setOnClickListener {
+            val fragmentManager = parentFragmentManager
+            fragmentManager.beginTransaction()
+                .replace(R.id.fcv_all_fragment,settingFragment)
+                .commit()
+        }
+        binding.cloFaq.setOnClickListener {
+            val fragmentManager = parentFragmentManager
+            fragmentManager.beginTransaction()
+                .replace(R.id.fcv_all_fragment,faqFragment)
+                .commit()
+        }
+
+
 
     }
 
