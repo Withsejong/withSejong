b9e17e80e2711e3ce1d2e9f4ae1d5ff7ba34709f
MisterJerry <misterjerry@sju.ac.kr>
2024-04-19 22:45:19 +0900

    나의정보 페이지 중앙 nickname_indicator 반영 및 회원탈퇴 api 설정

--- app/src/main/java/com/withsejong/mypage/myInformation/MyInformationFragment.kt
+++ app/src/main/java/com/withsejong/mypage/myInformation/MyInformationFragment.kt
@@ -1,12 +1,22 @@
 package com.withsejong.mypage.myInformation
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.withsejong.R
 import com.withsejong.databinding.FragmentMyInformationBinding
+import com.withsejong.retrofit.RetrofitClient
+import com.withsejong.retrofit.deleteAccountResponse
+import com.withsejong.start.LoginChoicePage
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class MyInformationFragment : Fragment() {
@@ -17,6 +27,43 @@ class MyInformationFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentMyInformationBinding.inflate(layoutInflater,container,false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val userInfoSharedPreferences = requireContext().getSharedPreferences("userInfo",Context.MODE_PRIVATE)
+        val tokenSharedPreferences = requireContext().getSharedPreferences("token",Context.MODE_PRIVATE)
+        binding.tvNicknameIndicator.text = "${userInfoSharedPreferences.getString("nickname","Error")}님"
+
+        //회원탈퇴
+        val intent = Intent(requireContext(),LoginChoicePage::class.java)
+
+        binding.btnDeleteId.setOnClickListener {
+
+            val accessToken = tokenSharedPreferences.getString("accessToken",null)
+            val loadid = userInfoSharedPreferences.getString("studentId",null)
+
+            RetrofitClient.instance.deleteAccount(accessToken.toString(),loadid.toString()).enqueue(object : Callback<deleteAccountResponse>{
+
+                override fun onResponse(
+                    call: Call<deleteAccountResponse>,
+                    response: Response<deleteAccountResponse>
+                ) {
+                    Log.d("MyInformationFragment_TAG", response.toString())
+
+                    if(response.isSuccessful){
+                        startActivity(intent)
+
+                    }
+                }
+
+                override fun onFailure(call: Call<deleteAccountResponse>, t: Throwable) {
+                    Log.d("MyInformationFragment_TAG", t.toString())
+                }
+
+            })
+        }
+
     }
 
 
