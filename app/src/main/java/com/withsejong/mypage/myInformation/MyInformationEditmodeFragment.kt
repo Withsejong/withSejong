9c62744946c06d47eafd3ab20400c428cc7996ca
MisterJerry <misterjerry@sju.ac.kr>
2024-04-20 00:14:02 +0900

    et를 빈 문자열로 세팅, tv와 et 쉐프에 저장된 걸로 반영, et의 hint로 이전 값 표시, savebtn 설정, savebtn 누를 시 값들 쉐프에 저장

--- app/src/main/java/com/withsejong/mypage/myInformation/MyInformationEditmodeFragment.kt
+++ app/src/main/java/com/withsejong/mypage/myInformation/MyInformationEditmodeFragment.kt
@@ -1,12 +1,12 @@
 package com.withsejong.mypage.myInformation
 
+import android.content.Context.MODE_PRIVATE
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.withsejong.R
-import com.withsejong.databinding.FragmentMyInformationBinding
 import com.withsejong.databinding.FragmentMyInformationEditmodeBinding
 
 
@@ -18,6 +18,46 @@ class MyInformationEditmodeFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentMyInformationEditmodeBinding.inflate(layoutInflater,container,false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val userInfoSharedPreferences = requireActivity().getSharedPreferences("userInfo",MODE_PRIVATE)
+        val saveNickname = userInfoSharedPreferences.getString("nickname","error")
+        val saveMajor = userInfoSharedPreferences.getString("major","error")
+        binding.tvNicknameIndicator.text = "${saveNickname}님"
+        binding.etNicknameIndicator.setText("")
+        binding.etMajorIndicator.setText("")
+
+        binding.etNicknameIndicator.hint = "$saveNickname"
+        binding.etMajorIndicator.hint = "$saveMajor"
+
+
+
+        //이전 페이지 fragment 객체 정의
+        val myInformationFragment = MyInformationFragment()
+
+        binding.ibtnBack.setOnClickListener {
+            val fragmentManager = parentFragmentManager.beginTransaction()
+            fragmentManager.replace(R.id.fcv_all_fragment,myInformationFragment).commit()
+        }
+        binding.tvSave.setOnClickListener {
+            val updateInformation = userInfoSharedPreferences.edit()
+            if(binding.etNicknameIndicator.text.length>0){
+                updateInformation.putString("nickname",binding.etNicknameIndicator.text.toString())
+                updateInformation.apply()
+            }
+            if(binding.etMajorIndicator.text.length>0){
+                updateInformation.putString("major",binding.etMajorIndicator.text.toString())
+                updateInformation.apply()
+            }
+            //TODO 나의 정보 업데이트 하는 api 연결
+
+            val fragmentManager = parentFragmentManager.beginTransaction()
+            fragmentManager.replace(R.id.fcv_all_fragment,myInformationFragment).commit()
+        }
+
     }
 
 
