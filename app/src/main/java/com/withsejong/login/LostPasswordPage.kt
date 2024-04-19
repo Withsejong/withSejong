ecb8b908e880224cf39a7a12481861a3e617d567
MisterJerry <misterjerry@sju.ac.kr>
2024-04-19 22:51:51 +0900

    "비번을 잊으셨나요?"를 클릭 할 경우 세종인 인증 페이지로 이동하게 변경

--- app/src/main/java/com/withsejong/login/LostPasswordPage.kt
+++ app/src/main/java/com/withsejong/login/LostPasswordPage.kt
@@ -3,9 +3,16 @@ package com.withsejong.login
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.JsonParser
 
 import com.withsejong.databinding.ActivityLostPasswordPageBinding
+import com.withsejong.retrofit.RetrofitClient
+import com.withsejong.retrofit.changeForgotPassword
 import com.withsejong.start.LoginStartPage
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class LostPasswordPage : AppCompatActivity() {
@@ -17,10 +24,39 @@ class LostPasswordPage : AppCompatActivity() {
         binding = ActivityLostPasswordPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val saveId = intent.getStringExtra("id")
+        val intent = Intent(this@LostPasswordPage, LoginPage::class.java)
+
+
         binding.btnNext.setOnClickListener {
-            val intent = Intent(this@LostPasswordPage, LoginPage::class.java)
-            startActivity(intent)
-            finish()
+
+            if(binding.etNewPasswordOkInput.text.length>0) {
+                val jsonObject=JSONObject()
+                jsonObject.put("studentId",saveId)
+                jsonObject.put("password",binding.etNewPasswordOkInput.text)
+                RetrofitClient.instance.changeForgotPassword(JsonParser.parseString(jsonObject.toString())).
+                enqueue(object : Callback<changeForgotPassword>{
+                    override fun onResponse(
+                        call: Call<changeForgotPassword>,
+                        response: Response<changeForgotPassword>
+                    ) {
+                        if(response.isSuccessful){
+                            startActivity(intent)
+                            finish()
+                        }
+                    }
+
+                    override fun onFailure(call: Call<changeForgotPassword>, t: Throwable) {
+                        TODO("Not yet implemented")
+                    }
+
+                })
+
+
+            }
+
+
+
         }
     }
 }
