e793e204c4a3852d3b313a55839f16349fc7e132
MisterJerry <misterjerry@sju.ac.kr>
2024-04-23 15:15:17 +0900

    gson 유연화 코드 설정

--- app/src/main/java/com/withsejong/retrofit/RetrofitClient.kt
+++ app/src/main/java/com/withsejong/retrofit/RetrofitClient.kt
@@ -1,5 +1,6 @@
 package com.withsejong.retrofit
 
+import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -13,6 +14,13 @@ object RetrofitClient {
         val logging = HttpLoggingInterceptor()
         logging.setLevel(HttpLoggingInterceptor.Level.BODY)
 
+        //gson 유연화 설정
+        //MalformedJsonException 때문에 설정
+
+        val gson = GsonBuilder()
+            .setLenient()
+            .create()
+
         //커스텀okhttp클라이언트 생성
         val okHttpClient = OkHttpClient.Builder()
             .addInterceptor(logging)
@@ -23,13 +31,15 @@ object RetrofitClient {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASEURL)
             .client(okHttpClient)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
 
         retrofit.create(Api::class.java)
     }
 
     val onlySejongAuth : Api by lazy {
+
+
         //로깅 인터셉터 설정
         val logging = HttpLoggingInterceptor()
         logging.setLevel(HttpLoggingInterceptor.Level.BODY)
