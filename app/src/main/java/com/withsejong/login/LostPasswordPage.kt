f9aed0bfb5145e3f8ce8aafc612b8e40be5fbefa
MisterJerry <misterjerry@sju.ac.kr>
2024-04-23 15:18:11 +0900

    greeting에 들어가는 것을 학번에서 닉네임으로 변경

--- app/src/main/java/com/withsejong/login/LostPasswordPage.kt
+++ app/src/main/java/com/withsejong/login/LostPasswordPage.kt
@@ -2,6 +2,7 @@ package com.withsejong.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.JsonParser
 
@@ -23,8 +24,11 @@ class LostPasswordPage : AppCompatActivity() {
         binding = ActivityLostPasswordPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val saveId = intent.getStringExtra("id")
-        binding.tvAccountOk.text = """${saveId}님, 세종인 인증이 완료되었습니다.
+        val saveId = intent.getStringExtra("studentId")
+        Log.d("LostPasswordPage_TAG_saveId", saveId.toString())
+        val saveNickname = intent.getStringExtra("nickname")
+
+        binding.tvAccountOk.text = """${saveNickname}님, 세종인 인증이 완료되었습니다.
             |새롭게 사용할 비밀번호를 입력해 주세요 :D""".trimMargin()
 
 
@@ -34,29 +38,28 @@ class LostPasswordPage : AppCompatActivity() {
 
         binding.btnNext.setOnClickListener {
 
+            //TODO 비번 같은지 확인하는 로직 필요
             if(binding.etNewPasswordOkInput.text.length>0) {
                 val jsonObject=JSONObject()
                 jsonObject.put("studentId",saveId)
                 jsonObject.put("password",binding.etNewPasswordOkInput.text)
+                Log.d("LostPasswordPage_TAG", jsonObject.toString())
                 RetrofitClient.instance.changeForgotPassword(JsonParser.parseString(jsonObject.toString())).
                 enqueue(object : Callback<ChangeForgotPassword>{
                     override fun onResponse(
                         call: Call<ChangeForgotPassword>,
                         response: Response<ChangeForgotPassword>
                     ) {
+                        Log.d("LostPasswordPage_TAG",response.toString())
                         if(response.isSuccessful){
                             startActivity(intent)
                             finish()
                         }
                     }
-
                     override fun onFailure(call: Call<ChangeForgotPassword>, t: Throwable) {
-                        //TODO("Not yet implemented")
+                        Log.d("LostPasswordPage_TAG", t.toString())
                     }
-
                 })
-
-
             }
 
 
