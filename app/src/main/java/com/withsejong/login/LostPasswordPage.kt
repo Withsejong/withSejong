24cc11df8530336c04bb17dc41b4b2c7553ea19a
MisterJerry <misterjerry@sju.ac.kr>
2024-04-24 21:24:16 +0900

    새로운 비번 입력받는 edittext 2개가 일치하지 않으면 넘어가지 않는 로직 추가

--- app/src/main/java/com/withsejong/login/LostPasswordPage.kt
+++ app/src/main/java/com/withsejong/login/LostPasswordPage.kt
@@ -3,6 +3,7 @@ package com.withsejong.login
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.JsonParser
 
@@ -37,9 +38,18 @@ class LostPasswordPage : AppCompatActivity() {
 
 
         binding.btnNext.setOnClickListener {
+            //다음으로 못넘어 가는 조건 : 2 비번이 다를 때, 둘중 하나라도 입력 안했을 때,
+            // (optional) 비번 규칙에 맞지 않을 때
+            //TODO 비번 칸 둘다 입력되었을 경우 버튼 색 변하게4
 
-            //TODO 비번 같은지 확인하는 로직 필요
-            if(binding.etNewPasswordOkInput.text.length>0) {
+            if(binding.etNewPasswordInput.text.toString()!=binding.etNewPasswordOkInput.text.toString()){
+                //우선 토스트 메시지로 표시
+                Toast.makeText(this,"비밀번호가 일치하지 않습니다!", Toast.LENGTH_SHORT).show()
+            }
+            else if(binding.etNewPasswordInput.text.length==0 || binding.etNewPasswordOkInput.text.length==0){
+                Toast.makeText(this,"비어있는 칸이 있습니다!", Toast.LENGTH_SHORT).show()
+            }
+            else{
                 val jsonObject=JSONObject()
                 jsonObject.put("studentId",saveId)
                 jsonObject.put("password",binding.etNewPasswordOkInput.text)
@@ -61,6 +71,9 @@ class LostPasswordPage : AppCompatActivity() {
                     }
                 })
             }
+
+
+
 
 
 
