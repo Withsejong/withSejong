c63a4994159c166a2c1ad912b80e0a9eb4a61ec3
MisterJerry <misterjerry@sju.ac.kr>
2024-04-24 21:24:37 +0900

    뒤로가기 시 넘어가는 페이지 정의

--- app/src/main/java/com/withsejong/login/AccountPage.kt
+++ app/src/main/java/com/withsejong/login/AccountPage.kt
@@ -2,10 +2,13 @@ package com.withsejong.login
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import com.withsejong.R
 import com.withsejong.databinding.ActivityAccountPageBinding
 import com.withsejong.databinding.ActivityLoginChoicePageBinding
+import com.withsejong.start.LoginChoicePage
+import kotlin.math.log
 
 
 class AccountPage : AppCompatActivity() {
@@ -17,6 +20,14 @@ class AccountPage : AppCompatActivity() {
         binding = ActivityAccountPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //비번 잊으셨나요 페이지에서 온 경우 다시 로그인페이지로
+        val loginPage = LoginPage()
+        val intentLostPassword = Intent(this@AccountPage,loginPage::class.java)
+
+        //회원가입 페이지에서 온 경우 login chice 페이지로 이동
+        val loginChoicePage = LoginChoicePage()
+        val intentSignup = Intent(this@AccountPage, loginChoicePage::class.java)
+
 
         val isSignup = intent.getStringExtra("isSignup").toString()
         binding.btnAuth.setOnClickListener {//세종인 인증 버튼 클릭시
@@ -25,5 +36,30 @@ class AccountPage : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
+        if(isSignup=="1"){ //회원가입 페이지에서 온 경우 loginchoice page로 이동
+            //휴대폰 뒤로가기를 누른 경우 이전 fragment로 돌아가는 행동 정의
+            val backActionCallback = object : OnBackPressedCallback(true){
+                override fun handleOnBackPressed() {
+                    startActivity(intentSignup)
+                    finish()
+                }
+            }
+            this@AccountPage.onBackPressedDispatcher.addCallback(this@AccountPage,backActionCallback)
+        }
+        else{//비번잊으셨나요 페이지에서 온 경우
+
+            //휴대폰 뒤로가기를 누른 경우 이전 fragment로 돌아가는 행동 정의
+            val backActionCallback = object : OnBackPressedCallback(true){
+                override fun handleOnBackPressed() {
+                    startActivity(intentLostPassword)
+                    finish()
+                }
+            }
+            this@AccountPage.onBackPressedDispatcher.addCallback(this@AccountPage,backActionCallback)
+        }
+
+
+
     }
 }
