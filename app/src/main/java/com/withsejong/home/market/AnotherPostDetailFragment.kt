99ab362d962c6951695d52c0fd78ab4b95d4fd41
MisterJerry <misterjerry@sju.ac.kr>
2024-04-25 18:21:36 +0900

    bottomsheetfragment와 연결 된 클래스

---
+++ app/src/main/java/com/withsejong/home/market/AnotherPostDetailFragment.kt
@@ -0,0 +1,25 @@
+package com.withsejong.home.market
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.withsejong.R
+
+
+class AnotherPostDetailFragment : BottomSheetDialogFragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_another_post_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //TODO 각 버튼에 해당하는 동작 만들 것
+    }
+
+}
