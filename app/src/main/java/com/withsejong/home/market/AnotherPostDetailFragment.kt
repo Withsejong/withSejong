f0442ce4c45f3df8154f39ee00f0573758c6bc14
MisterJerry <misterjerry@sju.ac.kr>
2024-05-02 22:11:46 +0900

    postdetail다이얼로그 상단 코너 둥글게 하는 코드 추가

--- app/src/main/java/com/withsejong/home/market/AnotherPostDetailFragment.kt
+++ app/src/main/java/com/withsejong/home/market/AnotherPostDetailFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.withsejong.R
 
@@ -19,6 +20,15 @@ class AnotherPostDetailFragment : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val bottomSheetDialog = dialog as BottomSheetDialog?
+        val bottomSheet = bottomSheetDialog?.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
+        bottomSheet?.setBackgroundResource(R.drawable.design_another_post_detail_clo)
+
+
+
+
+
         //TODO 각 버튼에 해당하는 동작 만들 것
     }
 
