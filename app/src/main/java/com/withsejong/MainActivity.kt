feb95fde271ebebcb0dc26ee0c212d93072b9134
MisterJerry <misterjerry@sju.ac.kr>
2024-05-11 20:53:20 +0900

    바텀nav에서 글쓰기 버튼 클릭시 게시글작성 activity로 이동하게 변경

--- app/src/main/java/com/withsejong/MainActivity.kt
+++ app/src/main/java/com/withsejong/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.withsejong
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.withsejong.databinding.MainActivityBinding
 import com.withsejong.home.HomeFragment
 import com.withsejong.home.PostData
 import com.withsejong.mypage.MypageMainFragment
+import com.withsejong.post.PostActivity
 import com.withsejong.post.PostFragment
 
 class MainActivity : AppCompatActivity() {
@@ -46,6 +48,8 @@ class MainActivity : AppCompatActivity() {
         val postFragment = PostFragment()
         val mypageMainFragment = MypageMainFragment()
         val chatFragment = ChatFragment()
+        val postActivity = PostActivity()
+        val intentPost = Intent(this,postActivity::class.java)
 
         supportFragmentManager.beginTransaction().add(R.id.fcv_all_fragment,homeFragment).commit()
 
@@ -54,7 +58,9 @@ class MainActivity : AppCompatActivity() {
             when(it.itemId){
                 R.id.home->supportFragmentManager.beginTransaction().replace(R.id.fcv_all_fragment,homeFragment).commit()
                 //R.id.search -> supportFragmentManager.beginTransaction().replace(R.id.fcv_all_fragment,searchFragment).commit()
-                R.id.post -> supportFragmentManager.beginTransaction().replace(R.id.fcv_all_fragment,postFragment).commit()
+                //R.id.post -> supportFragmentManager.beginTransaction().replace(R.id.fcv_all_fragment,postFragment).commit()
+                R.id.post->startActivity(intentPost)
+
                 R.id.chat -> supportFragmentManager.beginTransaction().replace(R.id.fcv_all_fragment,chatFragment).commit()
                 R.id.mypage->supportFragmentManager.beginTransaction().replace(R.id.fcv_all_fragment,mypageMainFragment).commit()
             }
