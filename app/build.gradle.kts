158ceb3bbdde0009c7c20d40e50ae443f362fdfe
MisterJerry <misterjerry@sju.ac.kr>
2024-05-11 20:41:34 +0900

    glide관련 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,6 +90,9 @@ dependencies {
     implementation("com.google.firebase:firebase-messaging-ktx:23.4.1")
     implementation("com.google.firebase:firebase-messaging:23.4.1")
 
+    //glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
