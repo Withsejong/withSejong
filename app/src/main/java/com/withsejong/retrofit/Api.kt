cd6cbeeb1a3f029d2f20ddf3647170529eabda0f
MisterJerry <misterjerry@sju.ac.kr>
2024-05-11 20:40:58 +0900

    게시글 추가 api 추가

--- app/src/main/java/com/withsejong/retrofit/Api.kt
+++ app/src/main/java/com/withsejong/retrofit/Api.kt
@@ -1,13 +1,17 @@
 package com.withsejong.retrofit
 
 import com.google.gson.JsonElement
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.PUT
+import retrofit2.http.Part
 import retrofit2.http.Query
 
 interface Api {
@@ -77,6 +81,14 @@ interface Api {
     fun findNickname(
         @Query("studentId") studentId: String
     ): Call<String>
+
+    @Multipart
+    @POST("/user/board/save")
+    fun makePost(
+        @Header("Authorization") accessToken: String,
+        @Part("request") request : RequestBody,
+        @Part file : List<MultipartBody.Part>
+        ):Call<MakePostResponse>
 
 
 }
