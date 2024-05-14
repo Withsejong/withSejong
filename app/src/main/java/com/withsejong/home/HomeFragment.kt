0ee3f92327e53c85db8deaf6ca5a60b3310a4bc5
MisterJerry <misterjerry@sju.ac.kr>
2024-05-14 15:09:51 +0900

    mockdata 주석처리 및 서버에서 받아온 데이터로 리사이클러뷰를 그리게 변경

--- app/src/main/java/com/withsejong/home/HomeFragment.kt
+++ app/src/main/java/com/withsejong/home/HomeFragment.kt
@@ -1,7 +1,9 @@
 package com.withsejong.home
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,14 +15,54 @@ import com.withsejong.R
 import com.withsejong.databinding.FragmentHomeBinding
 import com.withsejong.home.market.AnotherPostDetailFragment
 import com.withsejong.home.search.SearchActivity
+import com.withsejong.retrofit.LoadPostResponse
+import com.withsejong.retrofit.RetrofitClient
+import retrofit2.Response
 
 class HomeFragment : Fragment() {
     private lateinit var binding:FragmentHomeBinding
+
+    companion object{
+        val loadData = ArrayList<LoadPostResponse>()
+
+    }
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(layoutInflater,container,false)
+
+
+        var page :Int = 0
+
+        val tokenSharedPreferences = requireActivity().getSharedPreferences("token", Context.MODE_PRIVATE)
+        val accessToken:String = tokenSharedPreferences.getString("accessToken","").toString()
+        RetrofitClient.instance.loadPost(accessToken = "Bearer $accessToken", page = page).enqueue(
+            object : retrofit2.Callback<List<LoadPostResponse>>{
+                override fun onResponse(
+                    call: retrofit2.Call<List<LoadPostResponse>>,
+                    response: Response<List<LoadPostResponse>>
+                ) {
+//                    for(i:Int in 0 until (response.body()?.size ?: 0)){
+//                        loadData.add(response.body())
+//                    }
+                    Log.d("HomeFragment", response.toString())
+                    response.body()?.let { loadData.addAll(it) }
+
+
+                }
+
+                override fun onFailure(
+                    call: retrofit2.Call<List<LoadPostResponse>>,
+                    t: Throwable
+                ) {
+                    Log.d("HomeFragment", t.toString())
+                }
+
+
+            }
+        )
+
         return binding.root
     }
 
@@ -30,60 +72,60 @@ class HomeFragment : Fragment() {
         //더미데이터 생성
         //TODO 더미데이터 테스트 코드이므로 추후에 통신을 통해 리스트에 저장하는 것 구현할 것!
 
-        val mockData = ArrayList<PostData>()
-        mockData.add(PostData("코딩강화 파이썬",
-            arrayListOf("#지기전","#소융대"),
-            7,
-            10000)
-        )
-        mockData.add(PostData("소프트웨어 개념사전",
-            arrayListOf("#컴공","#소융대","#소웨","#전정통"),
-            5,
-            20000)
-        )
-        mockData.add(PostData("돼지책 파이썬",
-            arrayListOf("#컴공","#소융대","#소웨","#전정통"),
-            5,
-            20000)
-        )
-        mockData.add(PostData("코딩강화 파이썬",
-            arrayListOf("#지기전","#소융대"),
-            7,
-            10000)
-        )
-        mockData.add(PostData("소프트웨어 개념사전",
-            arrayListOf("#컴공","#소융대","#소웨","#전정통"),
-            5,
-            20000)
-        )
-        mockData.add(PostData("돼지책 파이썬",
-            arrayListOf("#컴공","#소융대","#소웨","#전정통"),
-            5,
-            20000)
-        )
-        mockData.add(PostData("코딩강화 파이썬",
-            arrayListOf("#지기전","#소융대"),
-            7,
-            10000)
-        )
-        mockData.add(PostData("소프트웨어 개념사전",
-            arrayListOf("#컴공","#소융대","#소웨","#전정통"),
-            5,
-            20000)
-        )
-        mockData.add(PostData("돼지책 파이썬",
-            arrayListOf("#컴공","#소융대","#소웨","#전정통"),
-            5,
-            20000)
-        )
+//        val mockData = ArrayList<PostData>()
+//        mockData.add(PostData("코딩강화 파이썬",
+//            arrayListOf("#지기전","#소융대"),
+//            7,
+//            10000)
+//        )
+//        mockData.add(PostData("소프트웨어 개념사전",
+//            arrayListOf("#컴공","#소융대","#소웨","#전정통"),
+//            5,
+//            20000)
+//        )
+//        mockData.add(PostData("돼지책 파이썬",
+//            arrayListOf("#컴공","#소융대","#소웨","#전정통"),
+//            5,
+//            20000)
+//        )
+//        mockData.add(PostData("코딩강화 파이썬",
+//            arrayListOf("#지기전","#소융대"),
+//            7,
+//            10000)
+//        )
+//        mockData.add(PostData("소프트웨어 개념사전",
+//            arrayListOf("#컴공","#소융대","#소웨","#전정통"),
+//            5,
+//            20000)
+//        )
+//        mockData.add(PostData("돼지책 파이썬",
+//            arrayListOf("#컴공","#소융대","#소웨","#전정통"),
+//            5,
+//            20000)
+//        )
+//        mockData.add(PostData("코딩강화 파이썬",
+//            arrayListOf("#지기전","#소융대"),
+//            7,
+//            10000)
+//        )
+//        mockData.add(PostData("소프트웨어 개념사전",
+//            arrayListOf("#컴공","#소융대","#소웨","#전정통"),
+//            5,
+//            20000)
+//        )
+//        mockData.add(PostData("돼지책 파이썬",
+//            arrayListOf("#컴공","#소융대","#소웨","#전정통"),
+//            5,
+//            20000)
+//        )
 
-        val homeAdapter = HomeAdapter(mockData)
+        val homeAdapter = HomeAdapter(loadData)
         binding.rcvSellList.adapter=homeAdapter
         binding.rcvSellList.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.VERTICAL,false)
         //어댑터의 요소들 클릭했을 경우
         homeAdapter.setItemClickListener(object:HomeAdapter.OnItemClickListener{
             override fun onClick(v: View, position: Int) {
-                Toast.makeText(requireContext(),mockData[position].name,Toast.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(),loadData[position].title,Toast.LENGTH_SHORT).show()
             }
 
         })
