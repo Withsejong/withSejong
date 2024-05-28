3e40f1a400162c50cc5dd5fa880480d32e439a56
MisterJerry <misterjerry@sju.ac.kr>
2024-05-28 22:13:52 +0900

    chatting fragment연결

--- app/src/main/java/com/withsejong/MainActivity.kt
+++ app/src/main/java/com/withsejong/MainActivity.kt
@@ -1,19 +1,13 @@
 package com.withsejong
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.telecom.Call
 import androidx.appcompat.app.AppCompatActivity
+import com.withsejong.chatting.ChatFragment
 import com.withsejong.databinding.MainActivityBinding
 import com.withsejong.home.HomeFragment
-import com.withsejong.home.PostData
 import com.withsejong.mypage.MypageMainFragment
 import com.withsejong.post.PostActivity
-import com.withsejong.post.PostFragment
-import com.withsejong.retrofit.LoadPostResponse
-import com.withsejong.retrofit.RetrofitClient
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,7 +25,6 @@ class MainActivity : AppCompatActivity() {
 
         val homeFragment = HomeFragment()
         //val searchFragment = SearchFragment()
-        val postFragment = PostFragment()
         val mypageMainFragment = MypageMainFragment()
         val chatFragment = ChatFragment()
         val postActivity = PostActivity()
@@ -46,7 +39,6 @@ class MainActivity : AppCompatActivity() {
                 //R.id.search -> supportFragmentManager.beginTransaction().replace(R.id.fcv_all_fragment,searchFragment).commit()
                 //R.id.post -> supportFragmentManager.beginTransaction().replace(R.id.fcv_all_fragment,postFragment).commit()
                 R.id.post->startActivity(intentPost)
-
                 R.id.chat -> supportFragmentManager.beginTransaction().replace(R.id.fcv_all_fragment,chatFragment).commit()
                 R.id.mypage->supportFragmentManager.beginTransaction().replace(R.id.fcv_all_fragment,mypageMainFragment).commit()
             }
