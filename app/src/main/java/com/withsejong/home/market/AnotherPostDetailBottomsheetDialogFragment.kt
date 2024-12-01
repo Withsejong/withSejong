735f0f612d13dc495b338e0b3906520eaa974e2e
MisterJerry <misterjerry@sju.ac.kr>
2024-12-01 17:12:21 +0900

    fix: 게시글 상세보기에서 채팅하기를 한 경우 상대에게 알림이 보내지지 않는 문제 해결

--- app/src/main/java/com/withsejong/home/market/AnotherPostDetailBottomsheetDialogFragment.kt
+++ app/src/main/java/com/withsejong/home/market/AnotherPostDetailBottomsheetDialogFragment.kt
@@ -23,26 +23,33 @@ import retrofit2.Retrofit
 class AnotherPostDetailBottomsheetDialogFragment : BottomSheetDialogFragment() {
 
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
 
-        val binding = FragmentAnotherPostDetailBottomsheetdialogBinding.inflate(layoutInflater,container,false)
+        val binding = FragmentAnotherPostDetailBottomsheetdialogBinding.inflate(
+            layoutInflater,
+            container,
+            false
+        )
         val userReportDialogFragment = UserReportDialogFragment()
 
         val bottomSheetDialog = dialog as BottomSheetDialog?
-        val bottomSheet = bottomSheetDialog?.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
+        val bottomSheet =
+            bottomSheetDialog?.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
         bottomSheet?.setBackgroundResource(R.drawable.design_another_post_detail_clo)
         val tokenSharedPreferences = requireContext().getSharedPreferences("token", MODE_PRIVATE)
-        val userInfoSharedPreferences = requireContext().getSharedPreferences("userInfo", MODE_PRIVATE)
+        val userInfoSharedPreferences =
+            requireContext().getSharedPreferences("userInfo", MODE_PRIVATE)
         val accessToken = tokenSharedPreferences.getString("accessToken", "")
         val studentId = userInfoSharedPreferences.getString("studentId", "")
 
         val postId = arguments?.getString("postId")
         val boardId = arguments?.getInt("boardId")
         val boardTitle = arguments?.getString("boardTitle")
-
-
-
 
 
         //TODO 각 버튼에 해당하는 동작 만들 것
@@ -53,21 +60,26 @@ class AnotherPostDetailBottomsheetDialogFragment : BottomSheetDialogFragment() {
             jsonObject.put("subscriber", postId)
             jsonObject.put("boardId", boardId)
 
-
-            val makeChattingRoomThread = Thread{
+            val makeChattingRoomThread = Thread {
                 val response = RetrofitClient.instance.makeChattingRoom(
                     "Bearer $accessToken",
-                    JsonParser.parseString(jsonObject.toString())).execute()
+                    JsonParser.parseString(jsonObject.toString())
+                ).execute()
 
-                if(response.code()==403){
+                if (response.code() == 403) {
 
-                }
-                else if(response.isSuccessful){
-                    Log.d("AnotherPostDetailBottomsheetDialogFragment_TAG",response.body().toString())
+                } else if (response.isSuccessful) {
+                    Log.d(
+                        "AnotherPostDetailBottomsheetDialogFragment_TAG",
+                        response.body().toString()
+                    )
                     //TODO roomID에 해당하는 채팅방으로 이동
-                    val intentChattingRoom = Intent(requireContext(), ChattingRoomActivity::class.java)
+                    val intentChattingRoom =
+                        Intent(requireContext(), ChattingRoomActivity::class.java)
                     intentChattingRoom.putExtra("roomId", response.body()?.id)
-                    intentChattingRoom.putExtra("boardTitle",boardTitle)
+                    intentChattingRoom.putExtra("boardTitle", boardTitle)
+                    intentChattingRoom.putExtra("publisher", studentId)
+                    intentChattingRoom.putExtra("subscriber", postId)
 
                     startActivity(intentChattingRoom)
                     dismiss()
@@ -84,7 +96,7 @@ class AnotherPostDetailBottomsheetDialogFragment : BottomSheetDialogFragment() {
             val reportBundle = Bundle()
             reportBundle.putInt("boardId", boardId!!)
             userReportDialogFragment.arguments = reportBundle
-            userReportDialogFragment.show(parentFragmentManager,userReportDialogFragment.tag)
+            userReportDialogFragment.show(parentFragmentManager, userReportDialogFragment.tag)
 
         }
 
