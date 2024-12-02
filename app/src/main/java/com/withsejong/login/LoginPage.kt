0e63e15c3cbee88f0d3f11c6587aea9b36712b9e
MisterJerry <misterjerry@sju.ac.kr>
2024-12-03 03:53:41 +0900

    fix: 불필요한 toast 메시지 제거

--- app/src/main/java/com/withsejong/login/LoginPage.kt
+++ app/src/main/java/com/withsejong/login/LoginPage.kt
@@ -12,7 +12,6 @@ import com.google.gson.JsonParser
 import com.withsejong.MainActivity
 import com.withsejong.R
 import com.withsejong.databinding.ActivityLoginPageBinding
-import com.withsejong.mypage.MypageMainFragment
 import com.withsejong.retrofit.LoginResponse
 import com.withsejong.retrofit.RetrofitClient
 import com.withsejong.start.LoginChoicePage
@@ -38,37 +37,34 @@ class LoginPage : AppCompatActivity() {
         val loginChoicePage = LoginChoicePage()
 
         //휴대폰 뒤로가기를 누른 경우 이전 fragment로 돌아가는 행동 정의
-        val backActionCallback = object : OnBackPressedCallback(true){
+        val backActionCallback = object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
-                val intent = Intent(this@LoginPage,loginChoicePage::class.java)
+                val intent = Intent(this@LoginPage, loginChoicePage::class.java)
                 startActivity(intent)
                 finish()
             }
         }
-        this@LoginPage.onBackPressedDispatcher.addCallback(this@LoginPage,backActionCallback)
-
-
+        this@LoginPage.onBackPressedDispatcher.addCallback(this@LoginPage, backActionCallback)
         //입력 감지
 
-        var idInputCheck:Int = 0
-        var pwInputCheck:Int = 0
-        binding.etStudentIdInput.addTextChangedListener(object : TextWatcher{
+        var idInputCheck: Int = 0
+        var pwInputCheck: Int = 0
+        binding.etStudentIdInput.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if(binding.etStudentIdInput.text.length>0){
-                    idInputCheck=1
-                    if(idInputCheck+pwInputCheck==2){
+                if (binding.etStudentIdInput.text.length > 0) {
+                    idInputCheck = 1
+                    if (idInputCheck + pwInputCheck == 2) {
                         binding.btnNext.setBackgroundResource(R.drawable.design_next_btn_input)
                     }
-                }
-                else{
-                    idInputCheck=0
+                } else {
+                    idInputCheck = 0
                     binding.btnNext.setBackgroundResource(R.drawable.design_next_btn_noinput)
                 }
-                Log.d("LoginPage_TAG", (idInputCheck+pwInputCheck).toString())
+                Log.d("LoginPage_TAG", (idInputCheck + pwInputCheck).toString())
 
             }
 
@@ -78,83 +74,101 @@ class LoginPage : AppCompatActivity() {
 
         })
 
-        binding.etStudentPasswordInput.addTextChangedListener(object:TextWatcher{
+        binding.etStudentPasswordInput.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if(binding.etStudentPasswordInput.text.length>0){
-                    pwInputCheck=1
-                    if(idInputCheck+pwInputCheck==2){
+                if (binding.etStudentPasswordInput.text.length > 0) {
+                    pwInputCheck = 1
+                    if (idInputCheck + pwInputCheck == 2) {
                         binding.btnNext.setBackgroundResource(R.drawable.design_next_btn_input)
                     }
-                }
-                else{
-                    pwInputCheck=0
+                } else {
+                    pwInputCheck = 0
                     binding.btnNext.setBackgroundResource(R.drawable.design_next_btn_noinput)
                 }
-                Log.d("LoginPage_TAG", (idInputCheck+pwInputCheck).toString())
-
+                Log.d("LoginPage_TAG", (idInputCheck + pwInputCheck).toString())
             }
 
             override fun afterTextChanged(s: Editable?) {
 
             }
-
         })
 
         binding.btnNext.setOnClickListener {
+            Toast.makeText(this, "로그인 진행중 입니다. 잠시 기다려주세요.", Toast.LENGTH_SHORT).show()
             val tokenSharedPreferences = getSharedPreferences("token", MODE_PRIVATE)
-            val fcmToken = tokenSharedPreferences.getString("fcmToken",null)
+            val fcmToken = tokenSharedPreferences.getString("fcmToken", null)
 
-            val jsonObject=JSONObject()
-            jsonObject.put("studentId",binding.etStudentIdInput.text)
-            jsonObject.put("password",binding.etStudentPasswordInput.text)
+            val jsonObject = JSONObject()
+            jsonObject.put("studentId", binding.etStudentIdInput.text)
+            jsonObject.put("password", binding.etStudentPasswordInput.text)
             jsonObject.put("fcmToken", fcmToken)
-            Toast.makeText(this, "$fcmToken", Toast.LENGTH_SHORT).show()
-
             RetrofitClient.instance.login(JsonParser.parseString(jsonObject.toString()))
-                .enqueue(object :Callback<LoginResponse>{
+                .enqueue(object : Callback<LoginResponse> {
                     override fun onResponse(
                         call: Call<LoginResponse>,
                         response: Response<LoginResponse>
                     ) {
-                        Log.d("LoginPage_TAG",response.toString())
+                        Log.d("LoginPage_TAG", response.toString())
 
-                        if(response.isSuccessful){
-                            val tokenSharedPreferences = getSharedPreferences("token",
-                                MODE_PRIVATE)
-                            val userInfoSharedPreferences = getSharedPreferences("userInfo",
-                                MODE_PRIVATE)
+                        if (response.isSuccessful) {
+                            val tokenSharedPreferences = getSharedPreferences(
+                                "token",
+                                MODE_PRIVATE
+                            )
+                            val userInfoSharedPreferences = getSharedPreferences(
+                                "userInfo",
+                                MODE_PRIVATE
+                            )
                             val tokenEditor = tokenSharedPreferences.edit()
                             val userInfoEditor = userInfoSharedPreferences.edit()
 
                             //토큰들 쉐프에 저장
-                            tokenEditor.putString("grantType", response.body()?.authToken?.grantType.toString())
-                            tokenEditor.putString("accessToken", response.body()?.authToken?.accessToken.toString())
-                            tokenEditor.putString("refreshToken", response.body()?.authToken?.refreshToken.toString())
+                            tokenEditor.putString(
+                                "grantType",
+                                response.body()?.authToken?.grantType.toString()
+                            )
+                            tokenEditor.putString(
+                                "accessToken",
+                                response.body()?.authToken?.accessToken.toString()
+                            )
+                            tokenEditor.putString(
+                                "refreshToken",
+                                response.body()?.authToken?.refreshToken.toString()
+                            )
                             tokenEditor.apply()
                             Log.d("LoginPage_TAG", response.body().toString())
-                            Log.d("LoginPage_TAG", tokenSharedPreferences.getString("accessToken", "def").toString())
+                            Log.d(
+                                "LoginPage_TAG",
+                                tokenSharedPreferences.getString("accessToken", "def").toString()
+                            )
                             //학생정보들 쉐프에 저장
-                            userInfoEditor.putString("studentId",response.body()?.studentId)
+                            userInfoEditor.putString("studentId", response.body()?.studentId)
                             userInfoEditor.putString("nickname", response.body()?.nickname)
-                            userInfoEditor.putString("major",response.body()?.major)
+                            userInfoEditor.putString("major", response.body()?.major)
                             userInfoEditor.apply()
 
-                            Log.d("LoginPage_TAG", tokenSharedPreferences.getString("refreshToken","Error").toString())
+                            Log.d(
+                                "LoginPage_TAG",
+                                tokenSharedPreferences.getString("refreshToken", "Error").toString()
+                            )
                             startActivity(intentNext)
                             finish()
-                        }
-                        else{
-                            Log.d("LoginPage_TAG",response.code().toString())
-                            Toast.makeText(this@LoginPage,"아이디 혹은 비밀번호를 확인해주세요!",Toast.LENGTH_SHORT).show()
+                        } else {
+                            Log.d("LoginPage_TAG", response.code().toString())
+                            Toast.makeText(
+                                this@LoginPage,
+                                "아이디 혹은 비밀번호를 확인해주세요!",
+                                Toast.LENGTH_SHORT
+                            ).show()
                         }
                     }
 
                     override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
-                        Log.d("LoginPage_TAG",t.toString())
+                        Log.d("LoginPage_TAG", t.toString())
 
                     }
                 })
