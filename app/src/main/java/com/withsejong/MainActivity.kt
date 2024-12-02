5649cb2127248e9125e0a7042c8564ac6f9a0b45
MisterJerry <misterjerry@sju.ac.kr>
2024-12-03 07:16:00 +0900

    feat: 로그인 이후 뒤로가기를 한번 더 눌러야 앱이 종료되는 기능 추가

--- app/src/main/java/com/withsejong/MainActivity.kt
+++ app/src/main/java/com/withsejong/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.withsejong
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.AlertDialog
 import com.withsejong.chatting.ChatFragment
 import com.withsejong.databinding.MainActivityBinding
 import com.withsejong.home.HomeFragment
@@ -27,6 +32,43 @@ class MainActivity : AppCompatActivity() {
 
         var fragmentManager = supportFragmentManager.beginTransaction()
         fragmentManager.add(R.id.fcv_all_fragment,homeFragment).commit()
+
+//        androidx.appcompat.app.AlertDialog.Builder(this)
+//            .setTitle("종료")
+//            .setMessage("세종끼리를 종료하시겠습니까?")
+//            .setPositiveButton("네",object:DialogInterface.OnClickListener{
+//                override fun onClick(dialog: DialogInterface?, which: Int) {
+//                    finish()
+//                }
+//
+//            })
+//            .setNegativeButton("아니오",object : DialogInterface.OnClickListener {
+//                override fun onClick(dialog: DialogInterface?, which: Int) {
+//                    dialog?.dismiss()
+//                }
+//
+//            }
+//
+//
+//            )
+//            .show()
+
+        //뒤로 가기 2번 눌러서 앱 종료 로직 추가
+
+        var backPressedTime = 0L
+        val onBackPressed = object : OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+                if(System.currentTimeMillis() - backPressedTime<=2000){
+                    finish() //앱 종료
+                }
+                else{
+                    backPressedTime=System.currentTimeMillis()
+                    Toast.makeText(this@MainActivity, "한번 더 누르면 종료합니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        }
+        onBackPressedDispatcher.addCallback(this@MainActivity,onBackPressed)
 
 
         //bottomnavigation 각 메뉴별로 클릭했을 때 fragment가 바뀌는 기능
