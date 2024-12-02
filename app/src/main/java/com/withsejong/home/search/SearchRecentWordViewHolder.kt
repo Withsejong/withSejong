8a676724cee167f2207791cd749c03111afc3cc2
MisterJerry <misterjerry@sju.ac.kr>
2024-12-03 06:32:17 +0900

    fix: 변수이름 오타 수정

--- app/src/main/java/com/withsejong/home/search/SearchRecentWordViewHolder.kt
+++ app/src/main/java/com/withsejong/home/search/SearchRecentWordViewHolder.kt
@@ -3,7 +3,7 @@ package com.withsejong.home.search
 import androidx.recyclerview.widget.RecyclerView
 import com.withsejong.databinding.ItemRecentSearchBinding
 
-class SearchRecentWordViewHolder(val bidning:ItemRecentSearchBinding):RecyclerView.ViewHolder(bidning.root) {
+class SearchRecentWordViewHolder(binding:ItemRecentSearchBinding):RecyclerView.ViewHolder(binding.root) {
 
-    val tv=bidning.tvRecentSearchName
+    val tv=binding.tvRecentSearchName
 }
